609831babd127e2ef40e28307164d2e6d68ab89f
Ruishan Lin <55273170+rs-lin@users.noreply.github.com>
2020-08-26 14:20:56 -0400

    Add files via upload
    initial

---
+++ GObot.py
@@ -0,0 +1,8 @@
+from deeppavlov import configs
+from deeppavlov.core.common.file import read_json
+
+new_config=read_json('gobot_dstc2_best.json')
+
+from deeppavlov.utils.telegram import interact_model_by_telegram
+
+interact_model_by_telegram(model_config=new_config, token="1152909597:AAFxbcctTbv36wGQa4O8kBU-bhLL-zJ8hzg")
